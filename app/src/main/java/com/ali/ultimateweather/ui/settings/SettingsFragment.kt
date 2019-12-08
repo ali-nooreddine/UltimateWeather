37a39ede9997f8536f028a10bd601d8bc45be3cd
alinoureddine <alinooreddine@gmail.com>
2019-12-09 00:27:39 +0200

    Initial commit

---
+++ app/src/main/java/com/ali/ultimateweather/ui/settings/SettingsFragment.kt
@@ -0,0 +1,13 @@
+package com.ali.ultimateweather.ui.settings
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+
+/**
+ * Created by Ali Noureddine on 2019-12-08.
+ */
+class SettingsFragment : Fragment() {
+    //PreferenceFragmentCompat() {
+    
+
+}
---
+++ app/src/main/java/com/ali/ultimateweather/ui/weather/future/list/FutureListWeatherViewModel.kt
@@ -0,0 +1,7 @@
+package com.ali.ultimateweather.ui.weather.future.list
+
+import androidx.lifecycle.ViewModel
+
+class FutureListWeatherViewModel : ViewModel() {
+    // TODO: Implement the ViewModel
+}
