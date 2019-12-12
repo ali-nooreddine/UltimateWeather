4afea8946bece13fdc9c9de7620c7c871836ccf5
Ali Noureddine <alinooreddine@gmail.com>
2019-12-12 10:20:27 +0200

    commit

--- app/src/main/java/com/ali/ultimateweather/ui/base/ScopedFragment.kt
+++ app/src/main/java/com/ali/ultimateweather/ui/base/ScopedFragment.kt
@@ -22,7 +22,7 @@ abstract class ScopedFragment : Fragment(), CoroutineScope {
     }
 
     override fun onDestroy() {
-        super.onDestroy()
         job.cancel()
+        super.onDestroy()
     }
 }
