06be5f4475f9420eb95b9babaa6ffd88b7b0715b
Ali Noureddine <alinooreddine@gmail.com>
2019-12-13 16:24:53 +0200

    commit

--- app/src/main/java/com/ali/ultimateweather/data/network/response/CurrentWeatherResponse.kt
+++ app/src/main/java/com/ali/ultimateweather/data/network/response/CurrentWeatherResponse.kt
@@ -1,14 +1,14 @@
 package com.ali.ultimateweather.data.network.response
 
 import com.ali.ultimateweather.data.db.entity.CurrentWeatherEntry
-import com.ali.ultimateweather.data.db.entity.Location
+import com.ali.ultimateweather.data.db.entity.WeatherLocation
 import com.ali.ultimateweather.data.db.entity.Request
 import com.google.gson.annotations.SerializedName
 
 
 data class CurrentWeatherResponse(
     val request: Request,
-    val location: Location,
+    val location: WeatherLocation,
     @SerializedName(value = "current")
     val currentWeatherEntry: CurrentWeatherEntry
 )
